12dae37e65eba60bb73e5eb639f337dbe65c4aab
Sarah Rohm <srohm@Rohmtown.local>
2011-05-07 11:14:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tron::Application.config.secret_token = '18f9b975a9a815fb2eb1ab01a7f032f5349b710a651035a8f05fdf6628900c36bdb4a48f737cf35f00614dcc78ce30c0d678a42f109a1d0f7c8abeed091075cc'
